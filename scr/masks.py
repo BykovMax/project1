9eb1b4641a4a107e9c7273ba04f8c431dbb8ea00
Bykov_Max <maksim.bykov.1997@mail.ru>
2025-04-25 01:45:53 +0300

    Добавлена функция get_date в модуле widjet. Исправлено название параметров в догстрингах в модуле masks. Исправлена опечатка в файле pyproject.toml из-за которой isort выдавал ошибку.

--- scr/masks.py
+++ scr/masks.py
@@ -5,13 +5,13 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
     """
     Принимает номер карты из 16 цифр и возвращает в формате "XXXX XX** **** XXXX", где Х это цифра номера карты.
 
-    :param card: Номер карты
+    :param card_number: Номер карты.
     :return: Возвращает маску номера карты.
     """
-    # Преобразование в строку если номер карты запрашивается не через input
+    # Преобразование в строку если номер карты запрашивается не через input.
     str_card = str(card_number)
 
-    # Проверка длины номера карты
+    # Проверка длины номера карты.
     if len(str_card) != 16:
         raise ValueError("Номер карты должен содержать 16 цифр.")
 
@@ -19,7 +19,7 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
     if not str_card.isdigit():
         raise ValueError("Номер карты должен содержать только цифры.")
 
-    # формирование маски карты
+    # формирование маски карты.
     mask_card = f"{str_card[:4]} {str_card[4:6]}** **** {str_card[-4:]}"
     return mask_card
 
@@ -28,13 +28,13 @@ def get_mask_account(account_number: Union[int, str]) -> str:
     """
     Принимает номер счета и скрывает его отображая в формате **XXXX, где Х это цифра номера.
 
-    :param account: Номер счета.
+    :param account_number: Номер счета.
     :return: Возвращает маску номера счета.
     """
-    # Преобразование в строку если номер счета запрашивается не через input
+    # Преобразование в строку если номер счета запрашивается не через input.
     str_account = str(account_number)
 
-    # Проверка длины номера счета
+    # Проверка длины номера счета.
     if len(str_account) != 20:
         raise ValueError("Номер счета должен содержать 20 цифр.")
 
@@ -42,6 +42,6 @@ def get_mask_account(account_number: Union[int, str]) -> str:
     if not str_account.isdigit():
         raise ValueError("Номер счета должен содержать только цифры.")
 
-    # формирование маски счета
+    # формирование маски счета.
     mask_account = f"*{str_account[-4:]}"
     return mask_account
--- scr/widjet.py
+++ scr/widjet.py
@@ -1,4 +1,6 @@
-from scr.masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+from scr.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number_card_or_account: str) -> str:
@@ -10,16 +12,42 @@ def mask_account_card(number_card_or_account: str) -> str:
     :return: Возвращает строку с наложенной маской на номер карты или счета.
     """
 
+    # Извлекаем номер.
     number = " ".join(number_card_or_account.split()[-1:])
 
+    # Накладывает маску на номер карты и выводит строку.
     if len(number) == 16:
         mask_card = get_mask_card_number(number)
         name_card = " ".join(number_card_or_account.split()[:-1])
         return f"{name_card} {mask_card}"
 
+    # Накладывает маску на номер счет и выводит строку.
     elif len(number) == 20:
         mask_account = get_mask_account(number)
         return f"Счет {mask_account}"
 
+    # Проверка на корректность данных.
     else:
         raise ValueError("Не удалось определить формат(карта или счет) или недопустимая длина")
+
+
+def get_date(date_iso: str) -> str:
+    """
+    Принимает на вход строку с датой в формате "ГГГГ-ММ-ДДTЧЧ:ММ:СС.ffffff" (ISO формат).
+    Где:
+      - ГГГГ — год
+      - ММ — месяц
+      - ДД — день
+      - T — указание начала времени, просто маркер-разделитель.
+      - ЧЧ — часы
+      - ММ — минуты
+      - СС — секунды
+      - ffffff — микросекунды
+    Возвращает строку с датой в формате "ДД.ММ.ГГГГ".
+
+    :param date_iso: Строка с датой в формате "ГГГГ-ММ-ДДTЧЧ:ММ:СС.ffffff".
+    :return: Строка с датой в формате "ДД.ММ.ГГГГ".
+    """
+
+    date = datetime.fromisoformat(date_iso)
+    return date.strftime("%d.%m.%Y")
