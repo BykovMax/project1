054869194fd06c7a533bb0e3f4c745227871691a
Bykov_Max <maksim.bykov.1997@mail.ru>
2025-05-02 08:55:36 +0300

    Дописана функция filter_by_state в модуле processing.py по правилам PEP8

--- scr/processing.py
+++ scr/processing.py
@@ -1,13 +1,16 @@
-def filter_by_state(list_dict, state="EXECUTED"):
-    """
-    Фильтрует список словарей
+from typing import Dict, List
 
-    :param list_dict:
-    :param state:
-    :return:
+
+def filter_by_state(list_dict: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
-    new_list_dict = []
-    for item in list_dict:
-        if item.get("state") == state:
-            new_list_dict.append(item)
-    return new_list_dict
+    Фильтрует список словарей по значению ключа 'state'.
+
+    :param list_dict: Список словарей.
+    :param state: Значение, по которому фильтровать (по умолчанию 'EXECUTED').
+    :return: Новый список отфильтрованных словарей.
+    """
+    new_list_dict = []  # Создаём пустой список для хранения результата
+    for item in list_dict:  # Проходим по каждому элементу в list_dict
+        if item.get("state") == state:  # Если значение по ключу 'state' совпадает с переданным
+            new_list_dict.append(item)  # Добавляем элемент в новый список
+    return new_list_dict  # Возвращаем отфильтрованный список
