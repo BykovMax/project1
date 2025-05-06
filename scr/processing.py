8a39a5d6dcac749ecfbb0949c4f0888b629f51b6
Bykov_Max <maksim.bykov.1997@mail.ru>
2025-05-06 21:33:44 +0300

    Исправлены ошибки

--- scr/processing.py
+++ scr/processing.py
@@ -13,7 +13,7 @@ def filter_by_state(list_dict: List[Dict], state: str = "EXECUTED") -> List[Dict
     """
     new_list_dict = []  # Создаём пустой список для хранения результата
     for item in list_dict:  # Проходим по каждому элементу в list_dict
-        if item.get("state") == state:  # Если значение по ключу 'state' совпадает с переданным
+        if "state" in  item and "state" in item["state"] == state:  # Если значение по ключу 'state' совпадает с переданным
             new_list_dict.append(item)  # Добавляем элемент в новый список
     return new_list_dict  # Возвращаем отфильтрованный список
 
@@ -21,9 +21,10 @@ def filter_by_state(list_dict: List[Dict], state: str = "EXECUTED") -> List[Dict
 def sort_by_date(list_dict: List[Dict], reverse: bool = True) -> List[Dict]:
     """
     Сортирует список словарей по ключу 'date'.
+    Формат даты должен быть: 'YYYY-MM-DDTHH:MM:SS.ffffff'.
 
     :param list_dict: Список словарей, содержащих ключ 'date'.
     :param reverse: Направление сортировки (по убыванию по умолчанию).
     :return: Новый список, отсортированный по дате.
     """
-    return sorted(list_dict, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%d"), reverse=reverse)
+    return sorted(list_dict, key=lambda x: datetime.strptime(x["date"], '%Y-%m-%dT%H:%M:%S.%f'), reverse=reverse)
