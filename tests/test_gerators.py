fd4eaf25267754c5754987f5abadc50f6f1e9ca0
Bykov_Max <maksim.bykov.1997@mail.ru>
2025-05-20 21:17:48 +0300

    Добавлена функция transaction_descriptions в модуль scr/generators.py и тесты к ней в tests/test_gerators.py

--- tests/test_gerators.py
+++ tests/test_gerators.py
@@ -1,6 +1,6 @@
 import pytest
 
-from scr.generators import filter_by_currency
+from scr.generators import filter_by_currency, transaction_descriptions
 
 # ==========================================
 # ====== Тесты для filter_by_currency ======
@@ -15,15 +15,15 @@ from scr.generators import filter_by_currency
             [873106923, 594226727],
         ),
         (
-            "RUB", # Фильтрация по RUB
+            "RUB",  # Фильтрация по RUB
             [873106923, 594226727],
         ),
         (
-            "USD", # Фильтрация по USD
+            "USD",  # Фильтрация по USD
             [939719570, 142264268, 895315941],
         ),
         (
-            "EUR", # Нет такого кода валюты
+            "EUR",  # Нет такого кода валюты
             [],
         ),
     ],
@@ -34,3 +34,39 @@ def test_filter_by_currency_parametrize(transactions, currency_code, expected_id
     assert result_id == expected_id
 
 
+# ================================================
+# ====== Тесты для transaction_descriptions ======
+# ================================================
+
+
+@pytest.mark.parametrize(
+    "expected_description",
+    [
+        [
+            "Перевод организации",
+            "Перевод со счета на счет",
+            "Перевод со счета на счет",
+            "Перевод с карты на карту",
+            "Перевод организации",
+        ]
+    ],
+)
+def test_transaction_descriptions(transactions, expected_description):
+    result = list(transaction_descriptions(transactions))
+    assert result == expected_description
+
+
+def test_transaction_descriptions_no_key():
+    transaction_without_description = [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            # "description" отсутствует
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        }
+    ]
+    with pytest.raises(KeyError):
+        list(transaction_descriptions(transaction_without_description))
